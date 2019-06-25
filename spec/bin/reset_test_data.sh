2c254b2572178a2755a177f3e6eb5b9f70993739
Harry Doan <hdoan741@gmail.com>
2019-06-25 18:34:56 -0500

    Update spec/bin/reset_test_data.sh
    Co-Authored-By: Andrew Nacin <andrewnacin@gmail.com>

--- spec/bin/reset_test_data.sh
+++ spec/bin/reset_test_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A simple script to reset test data for all rails versions
+# A simple script to reset test data for all Rails versions
 
 wd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RESET_TEST_DATA=true sh $wd/run_all_specs.sh
