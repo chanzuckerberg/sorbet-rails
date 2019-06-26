40eefb2a7b676e49dcc69674f4b92006895ad31c
Harry Doan <hdoan741@gmail.com>
2019-06-25 22:27:17 -0500

    Setup codecov to only run on Travis CI (#32)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 require "byebug"
 require "simplecov"
-require 'codecov'
 
 SimpleCov.start
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+if ENV["CI"] == "true"
+  require "codecov"
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 RSpec.configure do |config|
   config.expect_with(:rspec) { |c| c.syntax = :expect }
