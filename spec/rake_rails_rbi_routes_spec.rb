b26f07df0ce82ed6b35ce1953fb4b64651919742
Harry Doan <hdoan@summitps.org>
2019-06-27 16:01:12 -0700

    add specs for rake rails_rbi:routes

---
+++ spec/rake_rails_rbi_routes_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+require 'rake_helper'
+
+RSpec.describe 'rake rails_rbi:routes', type: :task do
+  it "preloads the Rails environment" do
+    expect(task.prerequisites).to include "environment"
+  end
+
+  it "generates routes.rbi correctly" do
+    task.invoke
+    expected_path = Rails.root.join("sorbet", "rails-rbi", "routes.rbi")
+    generated = File.read(expected_path)
+    expect_match_file(generated, 'expected_routes.rbi')
+  end
+end
