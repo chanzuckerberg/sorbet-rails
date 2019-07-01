1167ab245648d3c6d6a17d2af928efea33bee0bb
Harry Doan <hdoan@summitps.org>
2019-06-30 18:08:14 -0700

    fix extra "

--- lib/sorbet-rails/routes_rbi_formatter.rb
+++ lib/sorbet-rails/routes_rbi_formatter.rb
@@ -20,7 +20,7 @@ class RoutesRbiFormatter
       <<~MESSAGE
         # You do not have any routes defined!
         # Please add some routes in config/routes.rb.
-        # For more information about routes, see the Rails guide: https://guides.rubyonrails.org/routing.html."
+        # For more information about routes, see the Rails guide: https://guides.rubyonrails.org/routing.html.
       MESSAGE
   end
 
