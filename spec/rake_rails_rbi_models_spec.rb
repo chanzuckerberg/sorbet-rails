95ccd71f0431a174feaf969784ee8ef7e623b038
Harry Doan <hdoan@chanzuckerberg.com>
2019-07-08 17:18:07 -0700

    add spec

--- spec/rake_rails_rbi_models_spec.rb
+++ spec/rake_rails_rbi_models_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'rake rails_rbi:models', type: :task do
-  let!(:expected_dir_path) { Rails.root.join("sorbet", "rails-rbi", "models") }
+  let!(:generated_dir_path) { Rails.root.join("sorbet", "rails-rbi", "models") }
 
   it "preloads the Rails environment" do
     expect(task.prerequisites).to include("environment")
@@ -10,7 +10,7 @@ RSpec.describe 'rake rails_rbi:models', type: :task do
   it "generates model files correctly" do
     task.invoke
     expect_files(
-      base_dir: expected_dir_path,
+      base_dir: generated_dir_path,
       files: [
         'potion.rbi',
         'spell_book.rbi',
@@ -18,12 +18,30 @@ RSpec.describe 'rake rails_rbi:models', type: :task do
         'wizard.rbi',
       ]
     )
+    if Object.const_defined?('ActiveStorage')
+      expect_files(
+        base_dir: generated_dir_path.join('active_storage'),
+        files: [
+          'attachment.rbi',
+          'blob.rbi',
+        ],
+      )
+    end
+    if Object.const_defined?('ActiveRecord::InternalMetadata')
+      expect_files(
+        base_dir: generated_dir_path.join('active_record'),
+        files: [
+          'internal_metadata.rbi',
+          'schema_migration.rbi',
+        ]
+      )
+    end
   end
 
   it 'generates selected model correctly' do
     task.invoke("Wizard")
     expect_files(
-      base_dir: expected_dir_path,
+      base_dir: generated_dir_path,
       files: [
         'wizard.rbi',
       ]
@@ -33,7 +51,7 @@ RSpec.describe 'rake rails_rbi:models', type: :task do
   it 'generates more than 1 selected models correctly' do
     task.invoke("Wizard","SpellBook")
     expect_files(
-      base_dir: expected_dir_path,
+      base_dir: generated_dir_path,
       files: [
         'spell_book.rbi',
         'wizard.rbi',
