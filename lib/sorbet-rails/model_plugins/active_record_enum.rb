f0fe25701ba9a471a815fb0d6f1384f2d434456c
Harry Doan <hdoan@chanzuckerberg.com>
2019-07-21 19:11:28 -0700

    clean up and put base rbi at the bottom of the generated file

--- lib/sorbet-rails/model_plugins/active_record_enum.rb
+++ lib/sorbet-rails/model_plugins/active_record_enum.rb
@@ -3,37 +3,35 @@ class SorbetRails::ModelPlugins::ActiveRecordEnum < SorbetRails::ModelPlugins::B
   def generate(root)
     return unless model_class.defined_enums.size > 0
 
-    byebug
-    model_relation_class_name = model_relation_class_name
-    instance_module_name = model_module_name("EnumInstanceMethods")
+    enum_module_name = model_module_name("EnumInstanceMethods")
+    enum_module_rbi = root.create_module(name: enum_module_name)
 
-    model_class_rbi = root.create_class(name: model_class_name)
-    model_relation_shared_rbi = root.create_module(name: model_relation_shared_module_name)
-
-    instance_module_rbi = root.create_module(name: instance_module_name)
-    model_class_rbi.create_include(name: instance_module_name)
+    model_class_rbi = root.create_class(name: self.model_class_name)
+    model_relation_shared_rbi = root.create_module(name: self.model_relation_shared_module_name)
+    model_class_rbi.create_include(name: enum_module_name)
 
     # TODO: add any method for signature verification?
     model_class.defined_enums.each do |enum_name, enum_hash|
       model_class_rbi.create_method(
-        name: "#{enum_name.pluralize}",
+        name: enum_name.pluralize,
         return_type: "T::Hash[T.any(String, Symbol), Integer]",
+        class_method: true,
       )
       enum_hash.keys.each do |enum_val|
-        instance_module_rbi.create_method(
+        enum_module_rbi.create_method(
           name: "#{enum_val}?",
           return_type: "T::Boolean",
         )
-        instance_module_rbi.create_method(
+        enum_module_rbi.create_method(
           name: "#{enum_val}!",
           return_type: nil, # void
         )
         model_relation_shared_rbi.create_method(
-          name: "#{enum_val}",
+          name: enum_val,
           parameters: [
             Parameter.new(name: "*args", type: "T.untyped")
           ],
-          return_type: model_relation_class_name,
+          return_type: self.model_relation_class_name,
         )
         # force generating these methods because sorbet's hidden-definitions generate & override them
         model_class_rbi.create_method(
@@ -41,7 +39,8 @@ class SorbetRails::ModelPlugins::ActiveRecordEnum < SorbetRails::ModelPlugins::B
           parameters: [
             Parameter.new(name: "*args", type: "T.untyped")
           ],
-          return_type: model_relation_class_name,
+          return_type: self.model_relation_class_name,
+          class_method: true,
         )
       end
     end
