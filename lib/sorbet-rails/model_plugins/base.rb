10a7836eff3679e7fe4cf11852603aa0f7a58402
Harry Doan <hdoan@chanzuckerberg.com>
2019-07-28 17:34:00 -0700

    typedef plugins

--- lib/sorbet-rails/model_plugins/base.rb
+++ lib/sorbet-rails/model_plugins/base.rb
@@ -12,25 +12,22 @@ module SorbetRails::ModelPlugins
     # convenient rename
     Parameter = ::Parlour::RbiGenerator::Parameter
 
-    sig { returns(T.class_of(ActiveRecord::Base)) }
+    sig { implementation.returns(T.class_of(ActiveRecord::Base)) }
     attr_reader :model_class
 
-    sig { returns(T::Array[T.class_of(ActiveRecord::Base)]) }
+    sig { returns(T::Set[String]) }
     attr_reader :available_classes
 
     sig {
       params(
         model_class: T.class_of(ActiveRecord::Base),
-        available_classes: T::Array[T.class_of(ActiveRecord::Base)],
+        available_classes: T::Set[String],
       ).
       void
     }
     def initialize(model_class, available_classes)
       @model_class = T.let(model_class, T.class_of(ActiveRecord::Base))
-      @available_classes = T.let(available_classes, T::Array[T.class_of(ActiveRecord::Base)])
+      @available_classes = T.let(available_classes, T::Set[String])
     end
-
-    sig { abstract.params(root: Parlour::RbiGenerator::Namespace).void }
-    def generate(root); end
   end
 end
