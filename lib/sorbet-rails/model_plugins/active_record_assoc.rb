79e162343c6f8ad343a1dfcd96f2c0d9385c28dd
Harry Doan <hdoan@chanzuckerberg.com>
2019-07-28 18:12:56 -0700

    typedef more sigs in active_record_assoc plugin

--- lib/sorbet-rails/model_plugins/active_record_assoc.rb
+++ lib/sorbet-rails/model_plugins/active_record_assoc.rb
@@ -71,15 +71,18 @@ class SorbetRails::ModelPlugins::ActiveRecordAssoc < SorbetRails::ModelPlugins::
     )
   end
 
+  sig { params(reflection: T.untyped).returns(T.nilable(T::Boolean)) }
   def assoc_should_be_untyped?(reflection)
     polymorphic_assoc?(reflection) || !@available_classes.include?(reflection.klass.name)
   end
 
+  sig { params(reflection: T.untyped).returns(T.nilable(T::Boolean)) }
   def relation_should_be_untyped?(reflection)
     # only type the relation we'll generate
     assoc_should_be_untyped?(reflection) || !@available_classes.include?(reflection.klass.name)
   end
 
+  sig { params(reflection: T.untyped).returns(T.nilable(T::Boolean)) }
   def polymorphic_assoc?(reflection)
     reflection.through_reflection ?
       polymorphic_assoc?(reflection.source_reflection) :
