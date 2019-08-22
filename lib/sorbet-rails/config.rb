7c3b35b66f532f3c468afff80098bd235d1e3c8e
Connor Shea <connor.james.shea@gmail.com>
2019-08-21 20:42:08 -0600

    Add sigs for more of config.rb

--- lib/sorbet-rails/config.rb
+++ lib/sorbet-rails/config.rb
@@ -1,10 +1,14 @@
 # typed: true
 module SorbetRails
   class << self
-    def configure
+    extend T::Sig
+    
+    sig { params(blk: T.proc.params(arg0: Config).void).void }
+    def configure(&blk)
       yield config
     end
-    
+
+    sig { returns(Config) }
     def config
       @_config ||= Config.new
     end
@@ -13,6 +17,7 @@ module SorbetRails
   class Config
     extend T::Sig
 
+    sig { returns(T::Array[Symbol]) }
     attr_accessor :enabled_gem_plugins
 
     sig { void }
