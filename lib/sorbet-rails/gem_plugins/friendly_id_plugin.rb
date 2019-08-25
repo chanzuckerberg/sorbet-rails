180a0336d61722d93acf87835afc11e5cc77709e
Connor Shea <connor.james.shea@gmail.com>
2019-08-24 22:13:15 -0600

    Update FriendlyId plugin to use add_relation_query_method.

--- lib/sorbet-rails/gem_plugins/friendly_id_plugin.rb
+++ lib/sorbet-rails/gem_plugins/friendly_id_plugin.rb
@@ -4,17 +4,11 @@ class FriendlyIdPlugin < SorbetRails::ModelPlugins::Base
   def generate(root)
     return unless @model_class.singleton_class.included_modules.include?(::FriendlyId)
 
-    model_rbi = root.create_class(
-      model_class_name,
-      superclass: 'ApplicationRecord'
-    )
-
     # Friendly method definition
     # https://github.com/norman/friendly_id/blob/53f1ca01c5d9e71b62812028ff009fd138fd41b5/lib/friendly_id/base.rb#L216
-    model_rbi.create_method(
-      'friendly',
-      return_type: model_relation_class_name,
-      class_method: true
+    add_relation_query_method(
+      root,
+      'friendly'
     )
   end
 end
