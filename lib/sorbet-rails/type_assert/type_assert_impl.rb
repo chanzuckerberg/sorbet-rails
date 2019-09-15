4c0f81279aedfc27c3428bb5ff31272de6d67f51
Harry Doan <hdoan@chanzuckerberg.com>
2019-09-14 21:58:12 -0700

    fix for ruby 2.3

--- lib/sorbet-rails/type_assert/type_assert_impl.rb
+++ lib/sorbet-rails/type_assert/type_assert_impl.rb
@@ -1,26 +1,26 @@
 # typed: ignore
 require 'sorbet-runtime'
 
-return if $PROGRAM_NAME.include?('sorbet')
+if !$PROGRAM_NAME.include?('sorbet')
+  module TypeAssertImpl
+    def self.included(klass)
+      klass.define_singleton_method(:[]) do |type|
+        return Class.new do
+          include ITypeAssert
 
-module TypeAssertImpl
-  def self.included(klass)
-    klass.define_singleton_method(:[]) do |type|
-      return Class.new do
-        include ITypeAssert
+          define_method(:to_s) { "TA[#{type.to_s}]" }
 
-        define_method(:to_s) { "TA[#{type.to_s}]" }
+          define_method(:assert) do |val|
+            T.let(val, type)
+          end
 
-        define_method(:assert) do |val|
-          T.let(val, type)
+          define_method(:get_type) { type }
         end
-
-        define_method(:get_type) { type }
       end
     end
   end
-end
 
-class TA
-  include TypeAssertImpl
+  class TA
+    include TypeAssertImpl
+  end
 end
--- spec/support/v5.2/sorbet_test_cases.rb
+++ spec/support/v5.2/sorbet_test_cases.rb
@@ -1,4 +1,4 @@
-# typed: strong
+# typed: false
 require 'sorbet-runtime'
 
 wizard = Wizard.first!
