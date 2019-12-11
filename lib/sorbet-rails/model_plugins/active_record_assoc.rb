6ce306d09354863c03de4843092bdbf03e53d242
Harry Doan <hdoan@chanzuckerberg.com>
2019-12-11 09:46:03 -0800

    add a warning message when belongs_to is required at the db level but not rails level

--- lib/sorbet-rails/model_plugins/active_record_assoc.rb
+++ lib/sorbet-rails/model_plugins/active_record_assoc.rb
@@ -70,8 +70,13 @@ class SorbetRails::ModelPlugins::ActiveRecordAssoc < SorbetRails::ModelPlugins::
 
     if rails_required_config && !db_required_config
       puts "Warning: belongs_to association #{reflection.name} is required at the application
-        level but not at the DB level. It's recommended to enforce it by adding null & foreign key
-        constraint at the DB level.".squish!
+        level but **nullable** at the DB level. It's recommended to enforce it by adding
+        not-null & foreign key constraint at the DB level.".squish!
+    end
+    if !rails_required_config && db_required_config
+      puts "Warning: belongs_to association #{reflection.name} is specified as not-null at the
+        DB level but **not required** at the application level. It's recommended to enforce it by
+        specifying that the association is **required: true**.".squish!
     end
 
     rails_required_config || db_required_config
