e5dcd4aa3cce45ca2ef7b2bfb4c58aa1a2065be2
Harry Doan <hdoan@chanzuckerberg.com>
2020-01-27 17:09:35 -0800

    Use Rails.autoloaders to eagerload more stuff in Rails 6

--- lib/sorbet-rails/utils.rb
+++ lib/sorbet-rails/utils.rb
@@ -11,6 +11,11 @@ module SorbetRails::Utils
     # https://github.com/rails/rails/blob/master/railties/lib/rails/application/finisher.rb#L116
     # But this is not applied to Rails.application.eager_load! method
     Zeitwerk::Loader.eager_load_all if defined?(Zeitwerk)
+    # Let's eager_load more stuff so that we have the full picture of the runtime environment.
+    # Also only applicable to Rails 6.
+    if Rails.respond_to?(:autoloaders)
+      Rails.autoloaders.each(&:eager_load)
+    end
   end
 
   sig { params(method_name: String).returns(T::Boolean) }
