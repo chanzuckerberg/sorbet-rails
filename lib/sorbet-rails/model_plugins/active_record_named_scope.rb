d1b065b8bf33059032786411831a7b5e1810888d
Jeff Carbonella <jeff.carbonella@gusto.com>
2020-04-30 12:10:46 -0600

    Improve performance of model RBI generation (#321)
    This PR does two things that improve performance and memory usage:
    - Write the model RBI after each loop. Previously we were generating all the model RBIs and storing the strings inside of an array and then once we generated it all we were writing it out to files. This is pretty inefficient from a memory perspective which likely leads to a performance hit as well.
    - Speed up the `ActiveRecordNamedScope` plugin. I instrumented the plugins (just took `Time.now` before and after and printed it out) and was getting an average of ~4.5s for the ActiveRecordNamedScope. I found the culprit (`method_obj.owner.name` was very expensive for some reason 🤷) and took the time down to an average of 0.03s 🔥.
    
    I ran this against my company's codebase with ~750 models.
    
    **Before**
    Using `0.6.3` I actually never got a full time because it was taking too long so I killed it. It was definitely longer than 10 minutes
    
    **After**
    Using this branch:
    ```
    $ time bin/rails rails_rbi:all
    ... output ...
    
    real	3m23.620s
    user	2m37.853s
    sys	0m21.239s
    ```
    
    * Write the model RBI after each loop instead of at the end
    
    * Improve performance of NamedScopes plugin
    
    * Programmatically get methods defined in scoping/named.rb
    
    Co-authored-by: Harry Doan <hdoan@chanzuckerberg.com>

--- lib/sorbet-rails/model_plugins/active_record_named_scope.rb
+++ lib/sorbet-rails/model_plugins/active_record_named_scope.rb
@@ -7,15 +7,24 @@ class SorbetRails::ModelPlugins::ActiveRecordNamedScope < SorbetRails::ModelPlug
   def generate(root)
     model_class_rbi = root.create_class(self.model_class_name)
 
-    @model_class.methods.sort.each do |method_name|
+    # Named scope methods are dynamically defined by the `scope` method so their
+    # source_location is `lib/active_record/scoping/named.rb`. So we find scopes
+    # by two criteria:
+    # - they are defined in 'activerecord/lib/active_record/scoping/named.rb'
+    # - they are not one of the methods actually defined in that file's source.
+    # See: https://github.com/rails/rails/blob/master/activerecord/lib/active_record/scoping/named.rb
+    non_dynamic_methods = (
+      ActiveRecord::Scoping::Named::ClassMethods.instance_methods +
+      ActiveRecord::Scoping::Named::ClassMethods.protected_instance_methods +
+      ActiveRecord::Scoping::Named::ClassMethods.private_instance_methods
+    )
+
+    (@model_class.methods - non_dynamic_methods).sort.each do |method_name|
       next unless SorbetRails::Utils.valid_method_name?(method_name.to_s)
+
       method_obj = @model_class.method(method_name)
       next unless method_obj.present? && method_obj.source_location.present?
-      # we detect sscopes defined in a model by 2 criteria:
-      # - they don't have an owner name
-      # - they are defined in 'activerecord/lib/active_record/scoping/named.rb'
-      # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/scoping/named.rb
-      next unless method_obj.owner.name == nil
+
       source_file = method_obj.source_location[0]
       next unless source_file.include?("lib/active_record/scoping/named.rb")
 
