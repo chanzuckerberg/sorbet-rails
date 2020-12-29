79d953c64a03bd9f7ad082376f8db5bc938d3f82
Daniel Gilchrist <dan@tanda.co>
2020-12-29 12:28:40 +0000

    Fix issue in TStructComparable where we weren't comparing the props properly

--- spec/tstruct_comparable.rb
+++ spec/tstruct_comparable.rb
@@ -2,7 +2,7 @@ module TStructComparable
   def ==(other)
     return false unless other.is_a?(self.class)
 
-    self.class.props.keys.each do |prop|
+    self.class.props.keys.all? do |prop|
       self.send(prop) == other.send(prop)
     end
   end
