a3dc41877f3d2fed66e7ccee40e599c3b19ad2fc
Connor Shea <connor.james.shea@gmail.com>
2021-06-20 10:32:34 -0600

    Add plugin for AASM gem.
    This adds a few autogenerated methods that are created when a model has
    an AASM state machine.

---
+++ lib/sorbet-rails/gem_plugins/aasm_plugin.rb
@@ -0,0 +1,50 @@
+# typed: strong
+class AasmPlugin < SorbetRails::ModelPlugins::Base
+  sig { override.params(root: Parlour::RbiGenerator::Namespace).void }
+  def generate(root)
+    return unless @model_class.include?(::AASM)
+
+    model_rbi = root.create_class(
+      model_class_name
+    )
+
+    aasm_events = @model_class.aasm.events.map(&:name)
+    aasm_states = @model_class.aasm.states.map(&:name)
+
+    # If you have an event like :bazify, you get these methods:
+    # - `may_bazify?`
+    # - `bazify`
+    # - `bazify!`
+    # - `bazify_without_validation!`
+    aasm_events.each do |event|
+      model_rbi.create_method(
+        "may_#{event}?",
+        return_type: 'T::Boolean'
+      )
+
+      model_rbi.create_method(
+        event.to_s,
+        return_type: 'T::Boolean'
+      )
+
+      model_rbi.create_method(
+        "#{event}!",
+        return_type: 'T::Boolean'
+      )
+
+      model_rbi.create_method(
+        "#{event}_without_validation!",
+        return_type: 'T::Boolean'
+      )
+    end
+
+    # - If you have a state like :baz, you get these methods:
+    # - `baz?`
+    aasm_states.each do |state|
+      model_rbi.create_method(
+        "#{state}?",
+        return_type: 'T::Boolean'
+      )
+    end
+  end
+end
